80666e410fd5ee5224e7173d7b9e6acf9e3b031d
Divya Pundhir <76607386+divyapundhir21@users.noreply.github.com>
2021-12-04 11:44:54 +0530

    Add files via upload

---
+++ Print.cpp
@@ -0,0 +1,13 @@
+// Program to print something on the screen
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+// cout statement is used to print something on th screen
+cout<<"Radhey Radhey"<<endl; 
+
+//endl is an object which is used to take the cursor on the next line like escape sequesnve \n
+    return 0;
+}
