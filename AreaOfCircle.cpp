319dd767d816438b3f2f4afef0c8538e7d0d3a68
Divya Pundhir <76607386+divyapundhir21@users.noreply.github.com>
2021-12-04 21:23:40 +0530

    Add files via upload

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,18 @@
+// Program to find the area of circle
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+float radius;
+float areaOfCircle = 0.0;
+cout<<"Enter radius of Circle: ";
+cin>>radius;
+areaOfCircle = 3.14 * radius * radius;
+
+cout<<"Area of Circle is: "<<areaOfCircle;
+
+    return 0;
+}
