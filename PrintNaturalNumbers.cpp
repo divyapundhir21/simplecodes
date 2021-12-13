6ab10d864363cdc26a4804f76b278b6eff2bf753
Divya Pundhir <76607386+divyapundhir21@users.noreply.github.com>
2021-12-13 18:54:37 +0530

    Add files via upload

---
+++ PrintNaturalNumbers.cpp
@@ -0,0 +1,23 @@
+// Program to print n natural numbers
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+// variable declarations
+	int lastNumber;
+cout<<"Enter the target number: "; 
+cin>>lastNumber;
+
+for(int i=1;i<=lastNumber;i++)
+{
+	cout<<i<<" ";
+}
+
+/*
+
+
+*/
+    return 0;
+}
